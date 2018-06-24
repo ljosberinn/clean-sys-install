99d5ce0acd6f42388c6788dbb90c4bac6a7d631d
ljosberinn <admin@gerritalex.de>
2018-06-24 19:58:48 +0200

    typo /k missing

--- windows/run.bat
+++ windows/run.bat
@@ -1,1 +1,1 @@
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin && start cmd installDependencies.bat
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin && start cmd /k installDependencies.bat
