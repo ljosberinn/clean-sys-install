1dcfaf09a7c13ea84362631c3cd4dd8f1454a865
ljosberinn <admin@gerritalex.de>
2018-07-03 19:42:58 +0200

    added WinSCP

--- windows/02_installDependencies.bat
+++ windows/02_installDependencies.bat
@@ -1,3 +1,3 @@
 cinst -y visualstudiocode-insiders --pre
 
-cinst -y googlechrome;firefox;git;github;hyper;nodejs;7zip;notepadplusplus;spotify;composer;bitnami-xampp;firacode;openoffice;rambox
+cinst -y googlechrome;firefox;git;github;hyper;nodejs;7zip;notepadplusplus;spotify;composer;bitnami-xampp;firacode;openoffice;rambox;winscp
